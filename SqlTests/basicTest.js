21feef53a0a5305b0a5c30593aa299d2cd95239e
Christopher <laytonsunlimited@gmail.com>
2014-09-14 17:07:09 -0700

    Added basic testing forumla I like

---
+++ SqlTests/basicTest.js
@@ -0,0 +1,14 @@
+module.exports.beforeTest = function(obj){
+
+
+}
+
+module.exports.test = function(obj){
+
+
+}
+
+module.exports.afterTest = function(obj){
+
+
+}
---
+++ SqlTests/register.js
@@ -0,0 +1,7 @@
+/*Import test via require-module system and add them to the register*/
+var basicTest = require('./basicTest');
+
+
+module.exports.register = function(){
+  return [];
+};
