e2216712f2b1b08a6a6875dfa337ae2f05eb067e
Christopher <laytonsunlimited@gmail.com>
2014-09-17 21:27:22 -0700

    Fixed tests for table parser-  MySql

--- Parsers/tableParser.js
+++ Parsers/tableParser.js
@@ -1,9 +1,11 @@
-var stringUtils = require('stringUtils')
+var stringUtils = require('stringUtils');
 var parsers = {
   mySql : function(table){
     var columns = table.columns, primaryKey = '', foreignKeys = [], endOfLine = '';
     var sqlString = "CREATE TABLE " + stringUtils.toSqlCase(table.name) + ' ( ';    
     for(var column in columns){
+      column = columns[column];
+      endOfLine = '';
       if(column.priKey){
         primaryKey += column.name + ','; 
       }
@@ -11,34 +13,34 @@ var parsers = {
         foreignKeys.push(column);
       }
       if(column.nul){
-        endOfLine += ' NOT NULL ';
+        endOfLine += ' NOT NULL';
       }
       if(column.autoInc){
-        endOfLine += ' AUTO_INCREMENT ';
+        endOfLine += ' AUTO_INCREMENT';
       }
-      sqlString += column.name + ' ' + column.type + ' ' + endOfLine;
+      sqlString += stringUtils.toSqlCase(column.name) + ' ' + column.type + endOfLine + ',';
     }
-    sqlString = sqlString.splice(0, sqlString.length - 1);
+    sqlString = sqlString.slice(0, sqlString.length - 1);
     if(primaryKey.length > 0){
-      sqlString += ', PRIMARY KEY (' + primaryKey + ' ) ';
+      sqlString += ', PRIMARY KEY (' + primaryKey.slice(0, primaryKey.length -1) + ')';
     }
     if(foreignKeys[0]){
       foreignKeys.forEach(function(column){
-        sqlString += ', FOREIGN KEY ( ' + column.name + ' ) REFERENCES ' + column.fkTable + '( ' + column.fkColumn + ') '
+        sqlString += ', FOREIGN KEY ( ' + column.name + ' ) REFERENCES ' + column.fkTable + '( ' + column.fkColumn + ')';
       });
     } 
-    return sqlString;  
+    return sqlString + ')';  
   },
 
 
 
-}
+};
 
 
 
 module.exports.createTableSql = function(TestRunner,table){
   var connectionType = TestRunner.connection.connectionType;
-  return parsers[connectionType]();     
+  return parsers[connectionType](table);     
 };
 
 module.exports.createRowSql = function(dataRow){
