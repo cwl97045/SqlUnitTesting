3fe78600b6201156b38fd9fd0fc98b227c0624fb
Christopher <laytonsunlimited@gmail.com>
2014-09-17 21:27:22 -0700

    Fixed tests for table parser-  MySql

--- Testing/tableParserTest.js
+++ Testing/tableParserTest.js
@@ -3,6 +3,9 @@ var assert = require('chai').assert, Table = require('table'), Column = require(
 describe('Table Parser Test Suite', function(){
   it('Should parse a  simple table object down to a basic mySql query', function(){
     var newTable = new Table('user');
+    var mockTestRunner = {};
+    mockTestRunner.connection = {};
+    mockTestRunner.connection.connectionType = 'mySql';
     var idColumn = new Column('ID', 'INT');
     idColumn.notNull(true);
     idColumn.autoIncremant(true);
@@ -10,8 +13,8 @@ describe('Table Parser Test Suite', function(){
     var nameColumn = new Column('name', 'VARCHAR(35)');
     nameColumn.notNull(true);
     newTable.setColumns([idColumn, nameColumn]);
-    var tableSql = tableParser.createTableSql(newTable);	
-    assert.equal(tableSql,'CREATE TABLE USER\n(\nID INT AUTO_INCREMENT NOT NULL,\nNAME VARCHAR(35) NOT NULL,\nPRIMARY KEY (ID))');
+    var tableSql = tableParser.createTableSql(mockTestRunner, newTable);	
+    assert.equal(tableSql,'CREATE TABLE USER ( ID INT NOT NULL AUTO_INCREMENT,NAME VARCHAR(35) NOT NULL, PRIMARY KEY (ID))');
   });
 });
 
