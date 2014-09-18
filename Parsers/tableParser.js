cf7a10db448121a1c80bf7b88b0aa1e6b471fc0d
Christopher <laytonsunlimited@gmail.com>
2014-09-17 21:14:04 -0700

    Made mysql module for table parser

--- Parsers/tableParser.js
+++ Parsers/tableParser.js
@@ -1,12 +1,44 @@
-var stringUtils = require('stringUtils'), connections = require('propertiesParser'), conn;
-connections.readProperties(function(data,err){
-  if(err) throw err;
-  conn = data;
-});
-module.exports.createTableSql = function(table){
-  var columns = table.columns, sqlStart = 'CREATE TABLE ', pK = '', connectionType = conn[table.connection].connectionType, fk = [];
-  
-  return sqlStart;
+var stringUtils = require('stringUtils')
+var parsers = {
+  mySql : function(table){
+    var columns = table.columns, primaryKey = '', foreignKeys = [], endOfLine = '';
+    var sqlString = "CREATE TABLE " + stringUtils.toSqlCase(table.name) + ' ( ';    
+    for(var column in columns){
+      if(column.priKey){
+        primaryKey += column.name + ','; 
+      }
+      if(column.fK){
+        foreignKeys.push(column);
+      }
+      if(column.nul){
+        endOfLine += ' NOT NULL ';
+      }
+      if(column.autoInc){
+        endOfLine += ' AUTO_INCREMENT ';
+      }
+      sqlString += column.name + ' ' + column.type + ' ' + endOfLine;
+    }
+    sqlString = sqlString.splice(0, sqlString.length - 1);
+    if(primaryKey.length > 0){
+      sqlString += ', PRIMARY KEY (' + primaryKey + ' ) ';
+    }
+    if(foreignKeys[0]){
+      foreignKeys.forEach(function(column){
+        sqlString += ', FOREIGN KEY ( ' + column.name + ' ) REFERENCES ' + column.fkTable + '( ' + column.fkColumn + ') '
+      });
+    } 
+    return sqlString;  
+  },
+
+
+
+}
+
+
+
+module.exports.createTableSql = function(TestRunner,table){
+  var connectionType = TestRunner.connection.connectionType;
+  return parsers[connectionType]();     
 };
 
 module.exports.createRowSql = function(dataRow){
@@ -19,6 +51,9 @@ module.exports.createRowSql = function(dataRow){
   sqlStart += ' VALUES (' + values.slice(0, values.length - 1) + ')';
   return sqlStart;
 };	
+
+
+
 
 
 
--- models/table.js
+++ models/table.js
@@ -1,4 +1,4 @@
-module.exports = function(name, database, connection){
+module.exports = function(name){
    return {
      name : name,
      columns : [],
