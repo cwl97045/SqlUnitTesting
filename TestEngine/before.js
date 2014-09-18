17ea5e75bba389adc09ff463573ac73812c1f78a
Christopher <laytonsunlimited@gmail.com>
2014-09-17 21:46:17 -0700

    Made changes

--- TestEngine/before.js
+++ TestEngine/before.js
@@ -2,30 +2,6 @@ var tests = require('register').register(),
 TestRunner = require('TestRunner'), connections = require('propertiesParser'), conn, databaseInterfaces = require('DatabaseInterface')(),
 tableParser = require('tableParser');
 
-<<<<<<< HEAD
-connections.readProperties(function(data, err){
-  if(err) throw err;
-  conn = data
-});
-
-tests.forEach(function(item, index, array){
-  var Runner = item.beforeTest(new TestRunner());
-  Runner.tableDefinitions.forEach(function(item, index, array){
-    Runner.connection = conn[item.connection];
-    Runner.db = databaseInterfaces[Runner.connection.connectionType];
-    Runner.dbConnection = Runner.db.createConnection(Runner.connection.info);
-    var statement = tableParser.createTableSql(item);
-    //SETUP TO RUN AS A SCRIPT
-    Runner.db.executeQuery(Runner.dbConnection, statement, function(err, rows, fields){
-      if(err) throw err;
-    });
-  });
-  Runner.dataRows.forEach(function(item,index,array){
-    var statement = tableParser.createRowSql(item);
-    //SETUP TO RUN AS A SCRIPT
-    Runner.db.executeQuery(Runner.dbConnection, statement, function(err,rows,fields){
-      if(err) throw err;
-=======
 module.exports.connectionSetUp = function(Test){
   var setUp = Test.before(new TestRunner()), conn;
   connections.readProperties(function(data, err){
@@ -69,7 +45,6 @@ module.exports.before = function (TestRunner){
       Runner.db.executeQuery(Runner.dbConnection, statement, function(err,rows,fields){
         if(err) throw err;
       });
->>>>>>> Fixed some tests, need to work on Table Parser
     });
   }
   });
