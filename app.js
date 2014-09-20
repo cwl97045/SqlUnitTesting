4752c006a78878b01f3f3fb6c0835b31415c2be1
Christopher <laytonsunlimited@gmail.com>
2014-09-20 09:14:38 -0700

    Added test view layer, will start doing some clean up

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var router = express.Router();
 var fileUtils = require('FileUtility');
+
 
 app.set('view engine', 'ejs');
 
 var test = fileUtils.getNamesOfAllFilesInTheDirectory(fs, 'Tests');
-
-app.get('/', function(req, res){
-  //Greeting page/Test list view page
-  res.redirect('/test');
+test = test.	map(function(item){
+  return item.split('.')[0];
 });
 
-app.get('/test', function(req,res){
+router.get('/', function(req, res){
+  //Greeting page/Test list view page
   res.render('index', {tests: test});
 });
+
+router.get('/test/:testName', function(req, res){
+  res.send('This is the ' + req.params.testName + '!');
+});
+
+app.use('/', router);
 
 app.listen(3000);
