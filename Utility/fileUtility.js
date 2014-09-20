c09f66d38435cab1318573843f41ea5eb8b9cf77
Christopher <laytonsunlimited@gmail.com>
2014-09-20 11:51:53 -0700

    Fixed some things for jshint

--- Utility/fileUtility.js
+++ Utility/fileUtility.js
@@ -4,11 +4,11 @@ module.exports.getNamesOfAllFilesInTheDirectory = function(fs,dirPath){
 
 module.exports.createFileWithContent = function(fs,dirName,fileName, content, callback){
   fs.writeFile(dirName + fileName, content, callback);
-}
+};
 
 module.exports.grabFileContentsFromDirectoryByName = function(fs, dirName, fileName,encoding){
   if(!encoding){
     encoding = "UTF-8";
   }
   return fs.readFileSync(dirName + fileName, encoding);
-}
+};
